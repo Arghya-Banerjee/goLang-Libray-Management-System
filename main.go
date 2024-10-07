929ee4d6440995aa226f4f969bdf7c4beda62ae8
Arghya Banerjee <arghya.banerjee.dev@gmail.com>
2024-10-07 09:16:53 +0530

    Database Donegit status

--- main.go
+++ main.go
@@ -1,2 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"lms/utils" // Import your utils package
+	"log"
+)
+
+func main() {
+	// Initialize the database connection
+	db := utils.InitDB()
+
+	// Defer closing the connection until the main function finishes
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Fatal("Error closing the database: ", err)
+		} else {
+			fmt.Println("Database connection closed.")
+		}
+	}()
+
+	fmt.Println("Database connection successful!")
+}
--- utils/database.go
+++ utils/database.go
@@ -1,1 +1,37 @@
 package utils
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/denisenkom/go-mssqldb" // SQL Server driver
+)
+
+func InitDB() *sql.DB {
+
+	server := "localhost"    // Use "localhost" or "127.0.0.1"
+	port := 1433             // Default SQL Server port
+	user := "sa"             // SQL Server username
+	password := "qwaszx1234" // SQL Server password
+	database := "library_db" // Database name
+
+	// Connection string for MS SQL Server
+	connectionString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+		server, user, password, port, database)
+
+	// Open the connection
+	db, err := sql.Open("sqlserver", connectionString)
+	if err != nil {
+		log.Fatal("Error creating connection pool: ", err)
+	}
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error pinging database: ", err)
+	}
+
+	fmt.Println("Connected to MS SQL Server!")
+	return db
+}
